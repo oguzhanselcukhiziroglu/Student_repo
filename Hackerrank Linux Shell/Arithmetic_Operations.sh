4102e15ccd4958e8f6f7c0fc8d97a7a9cd0a25e1
oguzhanselcukhiziroglu <oshiziroglu@gmail.com>
2022-04-01 23:42:20 +0300

    Arithmetic_Operations added

---
+++ Hackerrank Linux Shell/Arithmetic_Operations.sh
@@ -0,0 +1,36 @@
+<<"COMMENT"
+A mathematical expression containing +,-,*,^, / and parenthesis will be provided. Read in the expression, then evaluate it. Display the result rounded to  decimal places.
+
+Constraints
+
+All numeric values are <= 999.
+
+Sample Input
+
+Sample Input 1
+
+5+50*3/20 + (19*2)/7
+Sample Input 2
+
+-105+50*3/20 + (19^2)/7
+Sample Input 3
+
+(-105.5*7+50*3)/20 + (19^2)/7
+Sample Output
+
+Sample Output 1
+
+    17.929
+Sample Output 2
+
+    -45.929
+Sample Output 3
+
+    22.146
+COMMENT
+
+#!/bin/bash
+
+read num
+echo $num | bc -l | xargs printf "%.3f"
+
