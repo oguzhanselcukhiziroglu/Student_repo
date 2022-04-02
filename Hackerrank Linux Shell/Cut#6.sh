a88cd77d6f40f4b5cd96ec39ff055d45051a75ce
oguzhanselcukhiziroglu <oshiziroglu@gmail.com>
2022-04-02 22:55:42 +0300

    Cut#6 added

---
+++ Hackerrank Linux Shell/Cut#6.sh
@@ -0,0 +1,27 @@
+<<'COMMENT'
+Print the characters from thirteenth position to the end.
+Input Format
+A text file with lines of ASCII text only.
+
+Output Format
+The output should contain N lines. For each input line, print the characters from thirteenth position to the end.
+
+Sample Input
+New York is a state in the Northeastern and Mid-Atlantic regions of the United States. 
+New York is the 27th-most extensive, the third-most populous populated of the 50 United States. 
+New York is bordered by New Jersey and Pennsylvania to the south.
+About one third of all the battles of the Revolutionary War took place in New York.
+Henry Hudson's 1609 voyage marked the beginning of European involvement with the area.
+
+Sample Output
+a state in the Northeastern and Mid-Atlantic regions of the United States. 
+the 27th-most extensive, the third-most populous populated of the 50 United States. 
+bordered by New Jersey and Pennsylvania to the south.
+ird of all the battles of the Revolutionary War took place in New York.
+'s 1609 voyage marked the beginning of European involvement with the area.
+COMMENT
+
+while read n 
+do
+echo $n | cut -c 13-
+done 
