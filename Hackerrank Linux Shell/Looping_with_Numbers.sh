d8c5b7ddafbc14155896a7393f474be6c09761cd
oguzhanselcukhiziroglu <oshiziroglu@gmail.com>
2022-04-05 23:55:36 +0300

    Looping-with-Numbers added

---
+++ Hackerrank Linux Shell/Looping_with_Numbers.sh
@@ -0,0 +1,24 @@
+<<'COMMENT'
+Use a for loop to display the natural numbers from  to .
+
+Output Format
+
+1
+2
+3
+4
+5
+.
+.
+.
+.
+.
+50
+COMMENT
+
+#!/bin/bash
+
+for num in {1..50}
+do
+    "$num"
+done
